9e12d3ba031514da6a985bdb87c42979e6d0d198
Tatsuki Adaniya <t.adaniya0624@gmail.com>
2015-03-03 01:28:13 -0800

    Initial commit

---
+++ node/HTML_LED_TEST/led_test_Mac.js
@@ -0,0 +1,64 @@
+var app = require('http').createServer(handler), 
+    io = require('../node_modules/socket.io').listen(app), 
+    fs = require('fs'),
+    firmata = require('../node_modules/firmata'),
+    //board = new firmata.Board('/dev/ttyACM0', arduinoReady);
+    board = new firmata.Board('/dev/tty.usbmodem1421', arduinoReady);
+
+ 
+var ledPin = 13;
+ 
+function arduinoReady(err) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log('Firmware: ' + board.firmware.name 
+      + '-' + board.firmware.version.major 
+      + '.' + board.firmware.version.minor);
+ 
+    var ledOn = true;
+    board.pinMode(ledPin, board.MODES.OUTPUT);
+}
+ 
+app.listen(8080);
+console.log("Listening on http://localhost:8080...");
+ 
+// directs page requests to html files
+ 
+function handler (req, res) {
+  fs.readFile(__dirname + '/led_test.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading html file');
+    }
+ 
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+ 
+// this handles socket.io comm from html files
+ 
+io.sockets.on('connection', function(socket) {
+    socket.send('connected...');
+    
+    socket.on('message', function(data) {
+        if (data == 'turn on') {
+            console.log('+');
+            board.digitalWrite(ledPin, board.HIGH);
+            socket.broadcast.send("let there be light!");
+        }
+        if (data == 'turn off') {
+            console.log('-');
+            board.digitalWrite(ledPin, board.LOW);
+            socket.broadcast.send("who turned out the light?");
+        }
+        return;
+    });
+ 
+    socket.on('disconnect', function() {
+        socket.send('disconnected...');
+    });
+});
---
+++ node/README.md
@@ -0,0 +1,30 @@
+# NodeArduino
+## Set up
+### node !!! Please use node v 0.12.0
+
+    curl -L git.io/nodebrew | perl - setup
+    sudo vim .bashrc
+add **export PATH=$HOME/.nodebrew/current/bin:$PATH** to the end of .bashrc
+
+    source .bashrc
+    nodebrew ls-remote
+    nodebrew install v0.12.0
+    nodebrew use v0.12.0
+    node -v
+
+### socket.io
+
+	npm install socket.io
+
+### serialport
+
+	npm install serialport
+
+### johnny-five
+
+    git clone git://github.com/rwldrn/johnny-five.git && cd johnny-five
+    npm install
+
+### firmata
+
+    npm install -g firmata
