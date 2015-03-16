35e8b354c8a4bb0de553afc32aec0f98709d549d
AdaniyaRasPi2 <t.adaniya0624@gmail.com>
2015-03-16 08:57:14 +0000

    add node I2C

--- node/I2C/i2c_test.js
+++ node/I2C/i2c_test.js
@@ -1,4 +1,4 @@
 var i2c = require('i2c');
-var device1 = new i2c(0x04, {device: '/dev/i2c-1', debug: false});
-//device1.setAddress(0x4);
+var device1 = new i2c(0x4, {device: '/dev/i2c-1', debug: false});
+device1.setAddress(0x4);
 device1.writeByte(0x2, function(err) { console.log("error"); console.log(err); });
